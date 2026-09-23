eab6f2de463d8b864e6399787780278c737b5613
landco-debug <landco82@gmail.com>
2026-09-23 23:24:05 +0400

    Add stable ASR model identifiers

---
+++ Sources/CribeCore/ASR/ASRModelID.swift
@@ -0,0 +1,7 @@
+/// Stable identifiers persisted in UserDefaults.
+///
+/// Built-in identifiers must never be renamed casually: they survive application updates.
+public enum ASRModelID {
+    public static let parakeet = "parakeet-v3"
+    public static let gigaAME2ERNNTQ8 = "gigaam-v3-e2e-rnnt-q8_0"
+}
